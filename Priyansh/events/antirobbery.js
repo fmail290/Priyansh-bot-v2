b99589393760ee3b185ec28b294e728501ea069a
fmail290 <fmail3988@gmail.com>
2025-04-13 07:40:45 +0600

    Update antirobbery.js

--- Priyansh/events/antirobbery.js
+++ Priyansh/events/antirobbery.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "guard",
     eventType: ["log:thread-admins"],
     version: "1.0.0",
-    credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+    credits: "Mritunjoy Mondol ",
     description: "Prevent admin changes",
 };
 
